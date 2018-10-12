4569285c33cce6367abaf8c1067ab4742abb32eb
Eddie Padin <eddie@entertainermedia.net>
2018-10-12 17:59:12 -0500

    - lifted counters component to app.js; - add onDelete; - added navbar as stateless functional component; - added count to navbar; - destructrued props;

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './components/navbar';
+import Counters from "./components/counters";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        counters: [
+            {
+                id: 1,
+                value: 4
+            },
+            {
+                id: 2,
+                value: 0
+            },
+            {
+                id: 3,
+                value: 0
+            },
+            {
+                id: 4,
+                value: 0
+            }
+        ]
+    };
+    
+    //handle the onClick event to increment the counter
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+    };
+    
+    handleReset = () => {
+        const counters = this.state.counters.map(counter => {
+            counter.value = 0;
+            return counter;
+        });
+        this.setState({ counters });
+    };
+    
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(
+            counter => counter.id !== counterId
+        );
+        this.setState({ counters });
+    };
+    
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar totalCounters={this.state.counters.filter(counter => counter.value > 0).length} />
+                <main className="container">
+                    <Counters counters={this.state.counters} onReset={ this.handleReset} onIncrement={this.handleIncrement} onDelete={this.handleDelete}/>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0
-  };
-
-  //handle the onClick event to increment the counter
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+
         {/*<ul>*/}
         {/*{this.state.tags.map(tag => (*/}
         {/*<li key={tag}>{tag}</li>*/}
@@ -32,14 +30,14 @@ class Counter extends Component {
   //set the badge classes
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   //format the counter
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
